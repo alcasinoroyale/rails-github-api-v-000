3fe1e2d92d1caa62da2dae33f2a8c92224683891
Alex Zdatny <alexzdatny@gmail.com>
2019-01-28 22:47:41 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -18,6 +18,7 @@ class RepositoriesController < ApplicationController
 
   def create
     user = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.body = {'name': params[:name]}.to_json
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
